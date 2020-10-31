7f003fc582e36e425592e5fa59f5025f74a641ef
fmgalib <fmgalib@gmail.com>
2020-10-31 18:47:53 +0600

    sign-in form design with custom button

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './signin-signup.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SigninSignupPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SigninSignupPage;
